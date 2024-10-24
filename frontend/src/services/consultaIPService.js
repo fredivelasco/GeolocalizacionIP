55e7a5f68819c7d514c9c444641ee62e05feeafc
fredivelasco <fredijavier@gmail.com>
2024-10-24 18:30:49 -0500

    V1.0.1.1

--- frontend/src/services/consultaIPService.js
+++ frontend/src/services/consultaIPService.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = "http://172.18.0.3:8080/api/consulta/";
+//const API_URL = "http://172.18.0.3:8080/api/consulta/";
+
+const API_URL = "http://backend:8080/api/consulta/";
 
 export const fetchConsultaIPData = async (direccionIP) => {
 
